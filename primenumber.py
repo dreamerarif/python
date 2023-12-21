ea7832a4f3c38391bdb32a5fd312b2317d03ad75
Md Arif Chowdhury <arifchowdhury.cse@gmail.com>
2023-12-21 21:31:37 +0600

    prime number using python

---
+++ primenumber.py
@@ -0,0 +1,13 @@
+num=3
+
+if num==1:
+    print(num, "is not a prime number")
+elif num > 1:
+    for i in range(2, num):
+        if(num%i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+else:
+    print("Invalid Number!!")
